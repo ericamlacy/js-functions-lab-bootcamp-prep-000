a3d296c0e196ee1e09c4d4a1d6f60f1628772f26
Erica Lacy <ericalacy7@gmail.com>
2018-09-19 23:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(){
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo(holiday, name){
+  console.log('Happy holidays, ${name}!')
+}
+function happyHolidaysTo(holiday, name){
+  console.log('${holiday}, ${name}!')
+}
+function holidayCountdown(holiday, days){
+  console.log('It's ${days} until ${holiday}!')
+}
