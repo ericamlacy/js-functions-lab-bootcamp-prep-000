d6b1a22a416ae77bb6a1858c0177a08890a9bca8
Erica Lacy <ericalacy7@gmail.com>
2018-09-20 00:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // write your code below!
 function happyHolidays(){
-  console.log("Happy holidays!")
+  console.log("Happy holidays!");
 }
 function happyHolidaysTo(holiday, name){
-  console.log('Happy holidays, ${name}!')
+  console.log('Happy holidays, ${name}!');
 }
 function happyHolidaysTo(holiday, name){
-  console.log('${holiday}, ${name}!')
+  console.log('${holiday}, ${name}!');
 }
 function holidayCountdown(holiday, days){
-  console.log('It's ${days} until ${holiday}!')
+  console.log('It's ${days} until ${holiday}!');
 }
